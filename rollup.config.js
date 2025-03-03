f6f8c4dd3b0dd9ed1a211ac4004e0a41ca7396fd
Greg Hesp <greg.hesp@gmail.com>
2025-03-03 09:44:10 +0000

    Update to React 19

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,38 @@
-import typescript from '@rollup/plugin-typescript';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import terser from '@rollup/plugin-terser';
-import postcss from 'rollup-plugin-postcss';
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
+import postcss from "rollup-plugin-postcss";
 
 export default {
-  input: 'src/ha-bambulab-cards.ts',
-  output: {
-    dir: 'dist',
-    format: 'es',
-    sourcemap: true,
-  },
+  input: "src/ha-bambulab-cards.ts",
+  output: [
+    {
+      file: "dist/ha-bambulab-cards.js",
+      format: "iife",
+      sourcemap: false,
+    },
+  ],
   plugins: [
     resolve({
-      extensions: ['.js', '.ts', '.tsx']
+      extensions: [".js", ".ts", ".tsx"],
     }),
     commonjs(),
     json(),
     postcss({
       inject: false,
-      extract: true
+      extract: true,
     }),
     typescript({
-      tsconfig: './tsconfig.json',
-      sourceMap: true,
+      tsconfig: "./tsconfig.json",
+      sourceMap: false,
       inlineSources: true,
-      jsx: 'react',
+      jsx: "react",
       declaration: false,
-      declarationMap: false
+      declarationMap: false,
     }),
     terser(),
   ],
-  preserveEntrySignatures: 'strict',
-}; 
+  preserveEntrySignatures: "strict",
+};
