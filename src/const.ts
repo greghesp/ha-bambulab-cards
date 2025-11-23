a5b0f11708e7b68f1fb42d2df5f5b15377208b4b
AdrianGarside <adriangarside@outlook.com>
2025-11-23 11:02:40 -0800

    Add H2D Pro support (#127)
    * Add H2D Pro support
    
    * Sort
    
    * Cleanup

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,29 @@
 export const PREFIX_NAME = "ha-bambulab";
 export const INTEGRATION_DOMAIN = "bambu_lab";
 export const MANUFACTURER = "Bambu Lab";
-export const PRINTER_MODELS = ["A1", "A1 MINI", "A1 Mini", "A1MINI", "A1Mini", "A1mini", "P1P", "P1S", "P2S", "X1C", "X1E", "H2C", "H2S", "H2D"];
-export const AMS_MODELS = ["AMS", "AMS Lite", "AMS 2 Pro", "AMS HT"];
-export const AMS_MODELS_WITH_EXTERNAL_SPOOL = ["AMS", "AMS Lite", "AMS 2 Pro", "AMS HT", "External Spool"];
+export const PRINTER_MODELS = [
+    "A1",
+    "A1 MINI", "A1 Mini", "A1MINI", "A1Mini", "A1mini",
+    "H2C",
+    "H2D",
+    "H2DPRO",
+    "H2S",
+    "P1P",
+    "P1S",
+    "P2S",
+    "X1C",
+    "X1E"
+];
+export const AMS_MODELS = [
+    "AMS",
+    "AMS Lite",
+    "AMS 2 Pro",
+    "AMS HT"
+];
+export const AMS_MODELS_WITH_EXTERNAL_SPOOL = [
+    "AMS",
+    "AMS Lite",
+    "AMS 2 Pro",
+    "AMS HT",
+    "External Spool"
+];
