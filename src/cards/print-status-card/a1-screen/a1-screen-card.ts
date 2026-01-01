2b68ae088da2169d0e978042751c16b66cf24055
AdrianGarside <adriangarside@outlook.com>
2025-12-31 22:54:24 -0800

    Sort external spools left to right (#137)
    * Sort external spools left to right
    
    * Remove logging

--- src/cards/print-status-card/a1-screen/a1-screen-card.ts
+++ src/cards/print-status-card/a1-screen/a1-screen-card.ts
@@ -892,6 +892,9 @@ export class A1ScreenCard extends LitElement {
     });
 
     // Add external spools at the end
+    externalSpools.sort((a, b) =>
+      a.spools[0].localeCompare(b.spools[0])
+    );
     this._amsList.push(...externalSpools);
     
     // Set selected AMS to the active one, or default to first if none are active
